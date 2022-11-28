5ab967491ffa493f8166d22a9db6c5a7c5e6c664
Bkhufa <bryankhufar@gmail.com>
2022-11-28 13:45:57 +0700

    Initial Commit

---
+++ IDNPreTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IDNPreTest
+//
+//  Created by Bryan Khufa on 28/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
